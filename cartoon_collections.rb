00dbb22177ffb6107e9835a9346c26abb2c5d3b0
sheesa azeeleeza <leezaluncheon@gmail.com>
2018-03-09 05:07:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
   array.each_with_index{|element,index| puts "#{index} #{element}"}
-
+end
 def summon_captain_planet(array)
   array.each.map{|element|element.capitalize<<!}
 end
